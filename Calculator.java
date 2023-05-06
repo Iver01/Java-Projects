9f2cc37ffe76cd5c1d70bf0872fed3b428ca02ad
Iver01 <123295884+Iver01@users.noreply.github.com>
2023-05-06 18:50:35 +0800

    Delete Calculator.java

--- Calculator.java
+++
@@ -1,8 +0,0 @@
-package Calculator;
-
-public class Calculator {
-	public static void main(String[] args) {
-		CalcuInterface Display = new CalcuInterface();
-		CalcuActionListeners Action = new CalcuActionListeners();
-	}
-}
