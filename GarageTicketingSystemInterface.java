1351313838478d055d12cdee4035b2bc4bbe8ecc
Iver01 <123295884+Iver01@users.noreply.github.com>
2023-05-14 00:11:41 +0800

    Update GarageTicketingSystemInterface.java

--- GarageTicketingSystemInterface.java
+++ GarageTicketingSystemInterface.java
@@ -1,153 +1,344 @@
-package GarageTicketingSystem;
-
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
+
 import java.awt.*;
-/*public JFrame frame;
-	    public JLabel headingLabel, nameLabel, fromLabel, destinationLabel, dateLabel, classLabel, amountLabel;
-	    public JTextField nameField, dateField, amountField;
-	    public JComboBox fromCombo, destCombo, classCombo;
-	    public JButton submitButton, resetButton;
-	    public String[] from = {"Cruz", "Town", "Tomay", "CampDangwa", "Shilan","Bahong", "Alno"};
-	    public String[] destinations = {"Cruz", "Town", "Tomay", "CampDangwa", "Shilan","Bahong", "Alno"};
-	    public String[] classes = {"Economy", "Business", "First Class"};*/
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GarageTicketingSystemInterface{
-    public JFrame frame;
-    public JLabel heading, name, source, destination, date, classLabel, amount;
-    public JTextField nameField, dateField, amountField;
-    public JComboBox sourceCombo, destCombo, classCombo;
-    public JButton submitButton, resetButton;
-    public String[] sources = {"New York", "Washington D.C.", "Los Angeles", "Chicago", "San Francisco"};
-    public String[] destinations = {"New York", "Washington D.C.", "Los Angeles", "Chicago", "San Francisco"};
-    public String[] classes = {"Economy", "Business", "First Class"};
-
-    GarageTicketingSystemInterface() {
-        frame = new JFrame("Travel Ticketing System");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 600);
-        frame.getContentPane().setBackground(Color.BLACK);
-        frame.setLayout(new BorderLayout());
-
-        heading = new JLabel("Book Your Ticket");
-        heading.setFont(new Font("Arial", Font.BOLD, 40));
-        heading.setForeground(Color.WHITE);
-        heading.setHorizontalAlignment(JLabel.CENTER);
-        heading.setVerticalAlignment(JLabel.CENTER);
-        frame.add(heading, BorderLayout.NORTH);
-
-        JPanel inputPanel = new JPanel();
-        inputPanel.setLayout(null);
-        inputPanel.setBounds(50, 100, 700, 400);
-        inputPanel.setBackground(Color.BLACK);
-
-        name = new JLabel("Name:");
-        name.setFont(new Font("Arial", Font.PLAIN, 20));
-        name.setForeground(Color.WHITE);
-        name.setBounds(50, 20, 100, 20);
-        inputPanel.add(name);
-
-        nameField = new JTextField();
-        nameField.setBounds(200, 20, 200, 25);
-        inputPanel.add(nameField);
-
-        source = new JLabel("Source:");
-        source.setFont(new Font("Arial", Font.PLAIN, 20));
-        source.setForeground(Color.WHITE);
-        source.setBounds(50, 70, 100, 20);
-        inputPanel.add(source);
-
-        sourceCombo = new JComboBox(sources);
-        sourceCombo.setBounds(200, 70, 200, 25);
-        inputPanel.add(sourceCombo);
-
-        destination = new JLabel("Destination:");
-        destination.setFont(new Font("Arial", Font.PLAIN, 20));
-        destination.setForeground(Color.WHITE);
-        destination.setBounds(50, 120, 150, 20);
-        inputPanel.add(destination);
-
-        destCombo = new JComboBox(destinations);
-        destCombo.setBounds(200, 120, 200, 25);
-        inputPanel.add(destCombo);
-
-        date = new JLabel("Date of Travel:");
-        date.setFont(new Font("Arial", Font.PLAIN, 20));
-        date.setForeground(Color.WHITE);
-        date.setBounds(50, 170, 150, 20);
-        inputPanel.add(date);
-
-        dateField = new JTextField();
-        dateField.setBounds(200, 170, 200, 25);
-        inputPanel.add(dateField);
-
-        classLabel = new JLabel("Class:");
-        classLabel.setFont(new Font("Arial", Font.PLAIN, 20));
-        classLabel.setForeground(Color.WHITE);
-        classLabel.setBounds(50, 220, 150, 20);
-        inputPanel.add(classLabel);
-
-        classCombo = new JComboBox(classes);
-        classCombo.setBounds(200, 220, 200, 25);
-        inputPanel.add(classCombo);
-
-        amount = new JLabel("Amount:");
-        amount.setFont(new Font("Arial", Font.PLAIN, 20));
-        amount.setForeground(Color.WHITE);
-        amount.setBounds(50, 270, 150, 20);
-        
-        amountField = new JTextField();
-        amountField.setBounds(200, 270, 200, 25);
-        amountField.setEditable(false);
-        frame.add(amountField);
-        
-        submitButton = new JButton("Submit");
-        submitButton.setBounds(50, 320, 150, 30);
-       
-        submitButton.setBackground(Color.WHITE);
-        submitButton.setForeground(Color.BLACK);
-        submitButton.setFocusPainted(false);
-        frame.add(submitButton);
-
-        resetButton = new JButton("Reset");
-        resetButton.setBounds(250, 320, 150, 30);
-        
-        resetButton.setBackground(Color.WHITE);
-        resetButton.setForeground(Color.BLACK);
-        resetButton.setFocusPainted(false);
-        frame.add(resetButton);
-
-        JPanel panel = new JPanel();
-        panel.setBackground(Color.BLACK);
-        panel.setLayout(null);
-        panel.add(heading);
-        panel.add(name);
-        panel.add(nameField);
-        panel.add(source);
-        panel.add(sourceCombo);
-        panel.add(destination);
-        panel.add(destCombo);
-        panel.add(date);
-        panel.add(dateField);
-        panel.add(classLabel);
-        panel.add(classCombo);
-        panel.add(amount);
-        panel.add(amountField);
-        panel.add(submitButton);
-        panel.add(resetButton);
-        frame.add(panel);
-
-        frame.setSize(600, 500);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-
-
-		    }
-
-
-
 	
-	   
+	public JFrame Frame = new JFrame("GARAGE TICKETING SYSTEM");
+	
+	public JLabel HeadingLabel, TicketTypeLabel;
+	public JLabel TaxLabel, SubTotalLabel, TotalLabel, Equals;
+	public JRadioButton StandardLabel, EconomyLabel, FirstClassLabel;
+	public JRadioButton SingleTicketLabel, ReturnTicketLabel;
+	public JTextField TaxTextField, SubTotalTextField, TotalTextField;
+	public JComboBox DestinationBox, DepartureBox, NumberTicketsBox;
+	public JButton SubmitButton, ResetButton, ExitButton;
+	
+	public JRadioButton creditCardRadioButton, paypalRadioButton;
+	
+	public JLabel ClassLabel, TicketLabel, NumberTicketLabel;
+	public JTextField ClassTextField, TicketTextField, NumberTicketTextField;
+	
+	public JLabel FromLabel, ToLabel, DateLabel, TimeLabel;
+	public JTextField FromTextField, ToTextField, DateTextField, TimeTextField;
+	public JLabel TicketNoLabel, PriceLabel, RouteLabel;
+	public JTextField TicketNoField, PriceTextField, RouteTextField;
+	
+	public JLabel Profile, Setting;
+	public JButton ViewProfile, ChangeName, ChangePass, LogOut;
+	
+	public JPanel TicketTypePanel, HeadingPanel, PaymentPanel, DestinationPanel, Print1Panel, 
+					Print2Panel, Print3Panel, ButtonPanel, MenuPanel;
+	
+	
+	public GarageTicketingSystemInterface() { 
+		//FRANE
+		Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Frame.setTitle("Travel Booking System");
+		Frame.setSize(1250, 600);
+		Frame.setLocationRelativeTo(null);
+		Frame.setResizable(false);
+		Frame.setLayout(new BorderLayout());;  
+		
+		//BORDER
+		Border raisedbevel = BorderFactory.createRaisedBevelBorder();
+		Border loweredbevel = BorderFactory.createLoweredBevelBorder();
+		Border blackline = BorderFactory.createLineBorder(Color.BLACK, 2);
+		Border loweredraisedbevel = BorderFactory.createCompoundBorder(raisedbevel, loweredbevel);;
+		Border loweredetched = BorderFactory.createEtchedBorder(EtchedBorder.LOWERED);
+		TitledBorder tickettpye; tickettpye = BorderFactory.createTitledBorder(loweredetched, "TICKET TYPE"); 
+		tickettpye.setTitleJustification(TitledBorder.CENTER); 
+		tickettpye.setTitleFont(new Font("Arial", Font.BOLD, 20));		
+		TitledBorder payment; payment = BorderFactory.createTitledBorder(loweredetched, "PAYMENT"); 
+		payment.setTitleJustification(TitledBorder.CENTER); 
+		payment.setTitleFont(new Font("Arial", Font.BOLD, 20));
+		TitledBorder destination; destination = BorderFactory.createTitledBorder(loweredetched, "DESTINATION"); 
+		destination.setTitleJustification(TitledBorder.CENTER); 
+		destination.setTitleFont(new Font("Arial", Font.BOLD, 20));
+		TitledBorder print1; print1 = BorderFactory.createTitledBorder(loweredetched, "TICKET SUMMARY"); 
+		print1.setTitleJustification(TitledBorder.RIGHT); 
+		print1.setTitleFont(new Font("Arial", Font.BOLD, 20));
+		TitledBorder print2; print2 = BorderFactory.createTitledBorder(loweredetched, "DEPARTURE"); 
+		print2.setTitleJustification(TitledBorder.RIGHT); 
+		print2.setTitleFont(new Font("Arial", Font.BOLD, 15));
+		TitledBorder print3; print3 = BorderFactory.createTitledBorder(loweredetched, "TICKET"); 
+		print3.setTitleJustification(TitledBorder.RIGHT); 
+		print3.setTitleFont(new Font("Arial", Font.BOLD, 15));
+		TitledBorder menu; menu = BorderFactory.createTitledBorder(loweredetched, "MENU"); 
+		menu.setTitleJustification(TitledBorder.CENTER); 
+		menu.setTitleFont(new Font("Arial", Font.BOLD, 20));
+		
+		//HEADING 
+		HeadingLabel = new JLabel("GARAGE TICKETING SYSTEM");
+		HeadingLabel.setBorder(loweredraisedbevel);
+		HeadingLabel.setFont(new Font("Arial", Font.BOLD, 60));
+		HeadingLabel.setForeground(Color.BLACK);
+		HeadingLabel.setHorizontalAlignment(JLabel.CENTER);
+		HeadingLabel.setVerticalAlignment(JLabel.CENTER);
+				
+		HeadingPanel = new JPanel();
+		HeadingPanel.setBounds(0, 0, 1200, 600);
+		HeadingPanel.add(HeadingLabel);
+				
+		//TICKET TYPE
+		StandardLabel = new JRadioButton("Standard");
+		StandardLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+		StandardLabel.setBounds(20, 40, 120, 20);
+		EconomyLabel = new JRadioButton("Economy");
+		EconomyLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+		EconomyLabel.setBounds(20, 80, 120, 20);
+		FirstClassLabel = new JRadioButton("First Class");
+		FirstClassLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+		FirstClassLabel.setBounds(20, 120, 120, 20);
+		SingleTicketLabel = new JRadioButton("Single Ticket");
+	    SingleTicketLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+	    SingleTicketLabel.setBounds(180, 40, 140, 20);
+	    ReturnTicketLabel = new JRadioButton("Return Ticket");
+	    ReturnTicketLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+	    ReturnTicketLabel.setBounds(180, 80, 150, 20);
+	    NumberTicketsBox = new JComboBox(new String[]{"Number of Tickets","1", "2", "2", "3", "4", "5"});  
+	    NumberTicketsBox.setFont(new Font("Arial", Font.PLAIN, 18));
+	    NumberTicketsBox.setBounds(170, 120, 190, 25);
+	    
+		TicketTypePanel = new JPanel();
+		TicketTypePanel.setLayout(null);
+		TicketTypePanel.setBounds(30, 100, 400, 170);
+		TicketTypePanel.setBorder(tickettpye);
+		TicketTypePanel.add(StandardLabel);
+		TicketTypePanel.add(EconomyLabel);
+		TicketTypePanel.add(FirstClassLabel);
+		TicketTypePanel.add(SingleTicketLabel);
+		TicketTypePanel.add(ReturnTicketLabel);
+		TicketTypePanel.add(NumberTicketsBox);
+		
+		//Destination
+		DestinationBox = new JComboBox(new String[]{"Destination", "Alno", "Bahong", "Alapang", "Cruz", "Tomay"});
+	    DestinationBox.setFont(new Font("Arial", Font.PLAIN, 18));
+	    DestinationBox.setBounds(230, 45, 150, 25);
+	    Equals = new JLabel("=");
+	    Equals.setFont(new Font("Arial", Font.BOLD, 20));
+	    Equals.setBounds(195, 45, 20, 25);
+	    DepartureBox = new JComboBox(new String[]{"Location", "Alno", "Bahong", "Alapang", "Cruz", "Tomay"});
+	    DepartureBox.setFont(new Font("Arial", Font.PLAIN, 18));
+	    DepartureBox.setBounds(20, 45, 150, 25);
+	    
+	    DestinationPanel = new JPanel();
+	    DestinationPanel.setLayout(null);
+	    DestinationPanel.setBounds(30, 280, 400, 100);
+	    DestinationPanel.setBorder(destination);
+	    DestinationPanel.add(DestinationBox);   
+	    DestinationPanel.add(DepartureBox);
+	    DestinationPanel.add(Equals);
+	    
+		//PAYMENT
+		TaxLabel = new JLabel("Tax");
+		TaxLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+		TaxLabel.setBounds(30, 30, 100, 20);
+		SubTotalLabel = new JLabel("SubTotal");
+		SubTotalLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+		SubTotalLabel.setBounds(30, 70, 100, 20);
+		TotalLabel = new JLabel("Total");
+		TotalLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+		TotalLabel.setBounds(30, 110, 100, 20);
+		TaxTextField = new JTextField();
+		TaxTextField.setBounds(220, 30, 150, 25);
+		TaxTextField.setEnabled(false);
+		SubTotalTextField  = new JTextField();
+		SubTotalTextField.setBounds(220, 70, 150, 25);
+		SubTotalTextField.setEnabled(false);
+		TotalTextField = new JTextField();
+		TotalTextField.setBounds(220, 110, 150, 25);
+		TotalTextField.setEnabled(false);
+		
+		PaymentPanel = new JPanel();
+		PaymentPanel.setLayout(null);
+		PaymentPanel.setBounds(30, 390, 400, 150);
+		PaymentPanel.setBorder(payment);
+		PaymentPanel.add(TaxLabel);
+		PaymentPanel.add(SubTotalLabel);
+		PaymentPanel.add(TotalLabel);
+		PaymentPanel.add(TaxTextField);
+		PaymentPanel.add(SubTotalTextField);
+		PaymentPanel.add(TotalTextField);
+	    
+		//PRINT1
+		ClassLabel = new JLabel("Class");
+		ClassLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+		ClassLabel.setHorizontalAlignment(JLabel.CENTER);
+		ClassLabel.setBounds(20, 40, 130, 20);
+		NumberTicketLabel = new JLabel("Number of Ticket");
+		NumberTicketLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+		NumberTicketLabel.setHorizontalAlignment(JLabel.CENTER);
+		NumberTicketLabel.setBounds(185, 40, 150, 20);
+		TicketLabel = new JLabel("Ticket");
+		TicketLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+		TicketLabel.setHorizontalAlignment(JLabel.CENTER);
+		TicketLabel.setBounds(370, 40, 150, 20);
+		ClassTextField  = new JTextField();
+		ClassTextField.setBounds(20, 70, 130, 30);
+		ClassTextField.setEnabled(false);
+		NumberTicketTextField  = new JTextField();
+		NumberTicketTextField.setBounds(185, 70, 150, 30);
+		NumberTicketTextField.setEnabled(false);
+		TicketTextField  = new JTextField();
+		TicketTextField.setBounds(370, 70, 150, 30);
+		TicketTextField.setEnabled(false);
+		
+		Print1Panel = new JPanel();
+		Print1Panel.setBounds(450, 100, 550, 120);
+		Print1Panel.setLayout(null);
+		Print1Panel.setBorder(print1);
+		Print1Panel.add(ClassLabel);
+		Print1Panel.add(TicketLabel);
+		Print1Panel.add(NumberTicketLabel);
+		Print1Panel.add(ClassTextField);
+		Print1Panel.add(TicketTextField);
+		Print1Panel.add(NumberTicketTextField);
+		
+		//PRINT2
+		FromLabel = new JLabel("From");
+		FromLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+		FromLabel.setBounds(30, 40, 50, 30);
+		ToLabel = new JLabel("To");
+		ToLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+		ToLabel.setBounds(30, 90, 50, 30);
+		DateLabel = new JLabel("Date");
+		DateLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+		DateLabel.setBounds(30, 140, 50, 30);
+		TimeLabel = new JLabel("Time");
+		TimeLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+		TimeLabel.setBounds(30, 190, 50, 30);
+		
+		FromTextField  = new JTextField();
+		FromTextField.setBounds(130, 40, 160, 30);
+		FromTextField.setEnabled(false);
+		ToTextField  = new JTextField();
+		ToTextField.setBounds(130, 90, 160, 30);
+		ToTextField.setEnabled(false);
+		DateTextField  = new JTextField();
+		DateTextField.setBounds(130, 140, 160, 30);
+		DateTextField.setEnabled(false);
+		TimeTextField  = new JTextField();
+		TimeTextField.setBounds(130, 190, 160, 30);
+		TimeTextField.setEnabled(false);
+		
+		Print2Panel = new JPanel();
+		Print2Panel.setBounds(450, 220, 320, 240);
+		Print2Panel.setLayout(null);
+		Print2Panel.setBorder(print2);
+		Print2Panel.add(FromLabel);
+		Print2Panel.add(ToLabel);
+		Print2Panel.add(DateLabel);
+		Print2Panel.add(TimeLabel);
+		Print2Panel.add(FromTextField);
+		Print2Panel.add(ToTextField);
+		Print2Panel.add(DateTextField);
+		Print2Panel.add(TimeTextField);
+		
+		//PRINT 3
+		TicketNoLabel = new JLabel("Ticket No");
+		TicketNoLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+		TicketNoLabel.setBounds(50, 20, 100, 30);
+		PriceLabel = new JLabel("Price");
+		PriceLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+		PriceLabel.setBounds(50, 90, 100, 30);
+		RouteLabel = new JLabel("Route");
+		RouteLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+		RouteLabel.setBounds(50, 160, 100, 30);
+		
+		TicketNoField  = new JTextField();
+		TicketNoField.setBounds(50, 50, 100, 30);
+		TicketNoField.setEnabled(false);
+		PriceTextField  = new JTextField();
+		PriceTextField.setBounds(50, 120, 100, 30);
+		PriceTextField.setEnabled(false);
+		RouteTextField  = new JTextField();
+		RouteTextField.setBounds(50, 190, 100, 30);
+		RouteTextField.setEnabled(false);
+		
+		Print3Panel = new JPanel();
+		Print3Panel.setBounds(770, 220, 230, 240);
+		Print3Panel.setLayout(null);
+		Print3Panel.setBorder(print3);
+		Print3Panel.add(TicketNoLabel);
+		Print3Panel.add(PriceLabel);
+		Print3Panel.add(RouteLabel);
+		Print3Panel.add(TicketNoField);
+		Print3Panel.add(PriceTextField);
+		Print3Panel.add(RouteTextField);
+
+		//BUTTONS
+		SubmitButton = new JButton("SUBMIT");
+		SubmitButton.setFont(new Font("Arial", Font.PLAIN, 20));	SubmitButton.setBounds(30, 13, 150, 40); 
+		SubmitButton.setBorder(loweredetched);
+		ResetButton = new JButton("RESET");
+		ResetButton.setFont(new Font("Arial", Font.PLAIN, 20));		ResetButton.setBounds(200, 13, 150, 40); 
+		ResetButton.setBorder(loweredetched);
+		ExitButton = new JButton("EXIT");
+		ExitButton.setFont(new Font("Arial", Font.PLAIN, 20));	ExitButton.setBounds(370, 13, 150, 40); 
+		ExitButton.setBorder(loweredetched);
+		
+		ButtonPanel = new JPanel();
+		ButtonPanel.setBounds(452, 475, 547, 64);
+		ButtonPanel.setLayout(null);
+		ButtonPanel.setBorder(loweredetched);
+		ButtonPanel.add(SubmitButton);
+		ButtonPanel.add(ResetButton);
+		ButtonPanel.add(ExitButton);
+		
+		//MENU
+		Profile = new JLabel("Profile");
+		Profile.setFont(new Font("Arial", Font.PLAIN, 20));
+		Profile.setBounds(25, 40, 130, 30);
+		Setting = new JLabel("Setting");
+		Setting.setFont(new Font("Arial", Font.PLAIN, 20));
+		Setting.setBounds(25, 120, 130, 30);
+		ViewProfile = new JButton("View Profile");
+		ViewProfile.setFont(new Font("Arial", Font.PLAIN, 15));	ViewProfile.setBounds(25, 80, 130, 20); 
+		ViewProfile.setBorder(loweredetched);
+		ChangeName = new JButton("Change Name");
+		ChangeName.setFont(new Font("Arial", Font.PLAIN, 15));	ChangeName.setBounds(25, 160, 130, 20); 
+		ChangeName.setBorder(loweredetched);
+		ChangePass = new JButton("Change Pass");
+		ChangePass.setFont(new Font("Arial", Font.PLAIN, 15));	ChangePass.setBounds(25, 200, 130, 20); 
+		ChangePass.setBorder(loweredetched);
+		LogOut = new JButton("Log Out");
+		LogOut.setFont(new Font("Arial", Font.PLAIN, 15));		LogOut.setBounds(25, 240, 130, 20); 
+		LogOut.setBorder(loweredetched);
+		
+		MenuPanel = new JPanel();
+		MenuPanel.setBounds(1020, 100, 180, 440);
+		MenuPanel.setLayout(null);
+		MenuPanel.setBorder(menu);
+		MenuPanel.add(Profile);
+		MenuPanel.add(ViewProfile);
+		MenuPanel.add(Setting);
+		MenuPanel.add(ViewProfile);
+		MenuPanel.add(ChangeName);
+		MenuPanel.add(ChangePass);
+		MenuPanel.add(LogOut);
+		
+		//FRAME
+		Frame.setSize(1250, 600);
+		Frame.setVisible(true);
+		
+		//ADD
+		Frame.add(MenuPanel);
+		Frame.add(ButtonPanel);
+		Frame.add(Print3Panel);
+		Frame.add(Print2Panel);
+		Frame.add(Print1Panel);
+		Frame.add(DestinationPanel);
+		Frame.add(PaymentPanel);
+        Frame.add(TicketTypePanel);	
+		Frame.add(HeadingPanel);
+		
+	}
 }
 
-		
-	
+
